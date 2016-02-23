fa8716fb68b22a6b92c76d4822b5cf7c22a250f3
manana <manana@student-PC>
2016-02-23 12:09:32 +0100

    Versión 4

--- 16_ejemplo_git/src/principal/Test.java
+++ 16_ejemplo_git/src/principal/Test.java
@@ -3,10 +3,12 @@ package principal;
 public class Test {
 
 	public static void main(String[] args) {
+		//Nueva versión desde otro eclipse
 		int a=10;
 		int b=5;
 		int c=7;
 		System.out.println(a+b+c);
+		System.out.println(a*b*c);
 	}
 
 }
